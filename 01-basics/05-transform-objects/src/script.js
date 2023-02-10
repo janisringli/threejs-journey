0bed7f7702a87e83f1d76106c58d38fe473a3cf5
“Janis <janis.ringli@gmail.com>
2023-02-11 00:03:50 +0100

    add group and lesson 5 done

--- 01-basics/05-transform-objects/src/script.js
+++ 01-basics/05-transform-objects/src/script.js
@@ -9,32 +9,31 @@ const scene = new THREE.Scene();
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-// mesh.position.x = 0.7
-// mesh.position.y = -0.6
-// mesh.position.z = 1
+const myGroup = new THREE.Group();
+scene.add(myGroup);
 
-mesh.position.set(0.7, -0.6, 1);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+myGroup.add(cube1);
 
-scene.add(mesh);
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "green" })
+);
+cube2.position.x = -2;
+myGroup.add(cube2);
 
-//Scale
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: "blue" })
+);
+cube3.position.x = +2;
+myGroup.add(cube3);
 
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 0.5;
 
-mesh.scale.set(2, 0.5, 0.5);
 
-//Rotation
-
-//180° = Math.PI
-//90° = Math.PI / 2
-// mesh.rotation.reorder("YXZ");
-mesh.rotation.y = Math.PI * 0.25;
-mesh.rotation.x = Math.PI * 0.25;
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper(2);
@@ -53,12 +52,9 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-//Lookat
-camera.lookAt(mesh.position);
 
 scene.add(camera);
 
-console.log(mesh.position.distanceTo(camera.position));
 /**
  * Renderer
  */
